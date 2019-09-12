527e584681aef0e1171273686945245cc5751741
BryanAdamss <BryanAdamss@foxmail.com>
2019-09-12 17:12:48 +0800

    :sparkles: 实现入口逻辑

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,22 @@
 import 'reflect-metadata'
 import { createConnection } from 'typeorm'
-import { User } from './entity/User'
+import * as express from 'express'
+import * as bodyParser from 'body-parser'
+import * as helmet from 'helmet'
+import * as cors from 'cors'
+
+const port = process.env.PORT || 3000
+const msg = `服务启动在${port}端口`
 
 createConnection()
   .then(async connection => {
-    console.log('Inserting a new user into the database...')
-    // const user = new User();
-    // user.firstName = "Timber";
-    // user.lastName = "Saw";
-    // user.age = 25;
-    // await connection.manager.save(user);
-    // console.log("Saved a new user with id: " + user.id);
+    console.log('database connected')
+    const app = express()
 
-    console.log('Loading users from the database...')
-    // const users = await connection.manager.find(User);
-    // console.log('Loaded users: ', users)
+    app.use(cors())
+    app.use(helmet())
+    app.use(bodyParser.json())
 
-    console.log('Here you can setup and run express/koa/any other framework.')
+    app.listen(port, () => console.log(msg))
   })
   .catch(error => console.log(error))
